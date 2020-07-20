eed8066d3d90858c28df1ef290f21f5ff28d77ba
Sophie Fuller <sophia.fuller@berkeley.edu>
2020-07-20 08:07:56 -0700

    hw05 release

---
+++ hw/hw05/setup/hw05.RAGS.R
@@ -0,0 +1,426 @@
+#################################################
+# Autograder tests for PH142 hw05-normal-binomial
+# This is the sanity check
+
+###############################################
+sol_path <- "setup/sol/"
+source("setup/autograder_setup.R")
+
+#source("../common/setup/autograder_setup.R")
+
+# Replace with number of problems
+setup_autograder(13)
+
+# --------------------------------------------
+check_problem1 = function() {
+  problem_num <- 1 # problem number
+  max_scores[problem_num] <<- 3 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+check_problem2 = function() {
+  problem_num <- 2 # problem number
+  max_scores[problem_num] <<- 1 # total pts possible
+  num_tests <<- 3 # num of checkpoints
+  
+  problem_types[problem_num] <<- "autograded" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  checkpoint(checkpoint_number = 1,
+             test = length(p2) == 2, 
+             correct_message = "Correct number of elements!",
+             error_message = "Incorrect number of elements.")
+  
+  checkpoint(checkpoint_number = 2,
+             test = p2[1] > 0.96 && p2[1] <0.98, 
+             correct_message = "Passed checkpoint for probability!",
+             error_message = "Incorrect number answer for part a.")
+  checkpoint(checkpoint_number = 3,
+             test =  p2[2] > 0 && p2[2] < 0.1,
+             correct_message = "Passed checkpoint for absolute difference!",
+             error_message = "Did you take the absolute difference?"
+            )
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+check_problem3 = function() {
+  problem_num <- 3 # problem number
+  max_scores[problem_num] <<- 3 # total pts possible
+  num_tests <<- 3 # num of checkpoints
+  
+  problem_types[problem_num] <<- "autograded" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  checkpoint(checkpoint_number = 1,
+             test = is.numeric(p3_lower_bound) & is.numeric(p3_upper_bound), 
+             correct_message = "You created two numeric objects!",
+             error_message = "Make sure you made two numeric objects.")
+  
+  checkpoint(checkpoint_number = 2,
+             test = p3_lower_bound < 70, 
+             correct_message = "Passed checkpoint for lower bound!",
+             error_message = "Incorect range. Check your answer.")
+  checkpoint(checkpoint_number = 3,
+             test =  p3_upper_bound > 70,
+             correct_message = "Passed checkpoint for upper bound!",
+             error_message = "Incorrect range. Check your answer."
+  )
+  
+  
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+# --------------------------------------------
+
+check_problem4 = function() {
+  problem_num <- 4 # problem number
+  max_scores[problem_num] <<- 3 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+check_problem5 = function() {
+  problem_num <- 5 # problem number
+  max_scores[problem_num] <<- 2 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+check_problem6 = function() {
+  problem_num <- 6 # problem number
+  max_scores[problem_num] <<- 1 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+check_problem7 = function() {
+  problem_num <- 7 # problem number
+  max_scores[problem_num] <<- 1 # total pts possible
+  num_tests <<- 1 # num of checkpoints
+  
+  problem_types[problem_num] <<- "autograded" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+   checkpoint(checkpoint_number = 1,
+             test = p7>0.05 && p7<0.2, 
+             correct_message = "You passed this checkpoint!",
+             error_message = "Did you use the correct function?")
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+check_problem8 = function() {
+  problem_num <- 8 # problem number
+  max_scores[problem_num] <<- 2 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+check_problem9 = function() {
+  problem_num <- 9 # problem number
+  max_scores[problem_num] <<- 1 # total pts possible
+  num_tests <<- 1 # num of checkpoints
+  
+  problem_types[problem_num] <<- "autograded" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  checkpoint(checkpoint_number = 1,
+             test = p9 > 0 && p9 < 0.02,
+             correct_message = "You passed this checkpoint",
+             error_message = "Did you use the correct function?")
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+check_problem10 = function() {
+  problem_num <- 10 # problem number
+  max_scores[problem_num] <<- 1 # total pts possible
+  num_tests <<- 1 # num of checkpoints
+  
+  problem_types[problem_num] <<- "autograded" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  checkpoint(checkpoint_number = 1,
+             test = p10 > 0 && p10 < 0.02,
+             correct_message = "You passed this checkpoint",
+             error_message = "Did you use the correct function?")
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+check_problem11 = function() {
+  problem_num <- 11 # problem number
+  max_scores[problem_num] <<- 4 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+
+check_problem12 = function() {
+  problem_num <- 12 # problem number
+  max_scores[problem_num] <<- 1 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+
+# --------------------------------------------
+
+
+check_problem13 = function() {
+  problem_num <- 13 # problem number
+  max_scores[problem_num] <<- 0 # total pts possible
+  num_tests <<- 0 # num of checkpoints
+  
+  problem_types[problem_num] <<- "free-response" # choices: autograded, free-response
+  problem_names[problem_num] <<- sprintf("Problem %d", problem_num)
+  
+  tests_failed <<- num_tests
+  
+  # Test cases here:
+  
+  # Assign appropriate score to problem depending on tests passed/failed
+  
+  if (tests_failed == 0 && problem_types[problem_num] != "free-response"){
+    scores[problem_num] <<- max_scores[problem_num]
+  } else {
+    scores[problem_num] <<- 0
+  }
+  
+  assert_that(tests_failed <= num_tests, tests_failed >= 0,
+              msg = sprintf("Did you set your num_test correctly for problem %d?", problem_num))
+  return_score(problem_num, num_tests, tests_failed)
+}
+
+# --------------------------------------------
+
+
+capture.output(check_problem1(), file='NUL')
+capture.output(check_problem2(), file='NUL')
+capture.output(check_problem3(), file='NUL')
+capture.output(check_problem4(), file='NUL')
+capture.output(check_problem5(), file='NUL')
+capture.output(check_problem6(), file='NUL')
+capture.output(check_problem7(), file='NUL')
+capture.output(check_problem8(), file='NUL')
+capture.output(check_problem9(), file='NUL')
+capture.output(check_problem10(), file='NUL')
+capture.output(check_problem11(), file='NUL')
+capture.output(check_problem12(), file='NUL')
+capture.output(check_problem13(), file='NUL')
+
+
